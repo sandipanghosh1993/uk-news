c11ceed26cd5e16010d68ac55635ff2c04f5756a
Sandipan Ghosh <sandipan.ghosh01@sap.com>
2020-08-23 02:55:00 +0530

    implemented API to fetch full content

--- server/server.js
+++ server/server.js
@@ -24,20 +24,24 @@ app.use((req, res, next) => {
   next();
 });
 
-const filterArticle = el => el.author && el.title && el.description;
+const filterArticle = el =>
+  el.author && el.title && el.description && el.urlToImage && el.publishedAt;
 
 app.get('/', async (req, res) => {
-  const defaultPageSize = 30;
-  const pageSize =
-    req.query.pageSize > defaultPageSize ? req.query.pageSize : defaultPageSize;
   try {
+    const defaultPageSize = 30;
+    const { pageSize, language, country } = req.query;
+    const newPageSize =
+      req.query.pageSize > defaultPageSize
+        ? req.query.pageSize
+        : defaultPageSize;
     const headlines = await newsapi.v2.topHeadlines({
-      pageSize: pageSize,
-      language: req.query.language,
-      country: req.query.country
+      pageSize: newPageSize,
+      language,
+      country
     });
     const newList = headlines.articles.filter(filterArticle);
-    return res.status(200).send(newList.slice(0, req.query.pageSize));
+    return res.status(200).send(newList.slice(0, pageSize));
   } catch (err) {
     return res.status(500).send('Server Error!');
   }
@@ -45,9 +49,10 @@ app.get('/', async (req, res) => {
 
 app.get('/search', async (req, res) => {
   try {
+    const { text, language } = req.query;
     const headlines = await newsapi.v2.everything({
-      q: req.query.text,
-      language: req.query.language
+      q: text,
+      language
     });
     const newList = headlines.articles.filter(filterArticle);
     return res.status(200).send(newList);
@@ -56,72 +61,17 @@ app.get('/search', async (req, res) => {
   }
 });
 
-// app.get('/news', async (req, res) => {
-//   const trump = "https://gizmodo.com/man-with-sole-authority-to-launch-nuclear-weapons-retwe-1844516233"
-//   const abc = 'https://news.google.com/__i/rss/rd/articles/CBMiK2h0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9TmJWd1FxS0JCTzjSAQA?oc=5'
-//   const theguardian = 'https://www.skysports.com/boxing/news/12183/12050898/whyte-vs-povetkin-dillian-whyte-will-be-defeated-by-alexander-povetkin-says-michael-hunter';
-//   try {
-//       const {url} = req.query;
-//       const doc = null;
-//       const text = await fetch(trump);
-//       const body = await text.text();
-//       res.status(200).send(body);
-//   } catch(err) {
-//       console.error(err)
-//       return res.status(500).send(err)
-//   }
-// });
-
-// To query /v2/top-headlines
-// All options passed to topHeadlines are optional, but you need to include at least one of them
-/*newsapi.v2
-  .topHeadlines({
-    sources: 'bbc-news,the-verge',
-    q: 'bitcoin',
-    category: 'business',
-    language: 'en',
-    country: 'us'
-  })
-  .then(response => {
-    console.log(response);
-
-//  });
-// To query /v2/everything
-// You must include at least one q, source, or domain
-/*newsapi.v2
-  .everything({
-    q: 'bitcoin',
-    sources: 'bbc-news,the-verge',
-    domains: 'bbc.co.uk, techcrunch.com',
-    from: '2017-12-01',
-    to: '2017-12-12',
-    language: 'en',
-    sortBy: 'relevancy',
-    page: 2
-  })
-  .then(response => {
-    console.log(response);
-
-    {
-      status: "ok",
-      articles: [...]
-    }
-
-});*/
-// To query sources
-// All options are optional
-/*newsapi.v2
-  .sources({
-    category: 'technology',
-    language: 'en',
-    country: 'us'
-  })
-  .then(response => {
-    console.log(response);
-
-  });*/
-
-// // 085579c16f7a4c43b7ed23a199f8305e
+app.get('/fullarticle', async (req, res) => {
+  try {
+    const { url } = req.query;
+    const response = await fetch(url);
+    const body = await response.text();
+    res.status(200).send(body);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send('Server Error!');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server started at ${port}`);
