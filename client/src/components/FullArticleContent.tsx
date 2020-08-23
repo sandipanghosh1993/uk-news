6c8967a53d7f74300b6e9068eb3de1c545951db1
Sandipan Ghosh <sandipan.ghosh01@sap.com>
2020-08-23 14:22:40 +0530

    made FullArticleContent a functional content

--- client/src/components/FullArticleContent.tsx
+++ client/src/components/FullArticleContent.tsx
@@ -1,45 +1,36 @@
 import React from 'react';
 import { Spinner } from 'react-bootstrap';
-import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 interface FullArticleContentProps {
   fullArticleContent: any;
 }
 
-interface FullArticleContentState {}
-
-class FullArticleContent extends React.PureComponent<
-  FullArticleContentProps,
-  FullArticleContentState
-> {
-  public constructor(props: FullArticleContentProps) {
-    super(props);
-  }
-  public render() {
-    if (!this.props.fullArticleContent) {
-      return (
-        <div style={{ textAlign: 'center', height: '50vh', marginTop: '25vh' }}>
-          <Spinner animation="grow" size="sm" />{' '}
-          <Spinner animation="grow" size="sm" />{' '}
-          <Spinner animation="grow" size="sm" />{' '}
-        </div>
-      );
-    }
+const FullArticleContent: React.FC<FullArticleContentProps> = (
+  props: FullArticleContentProps
+) => {
+  if (!props.fullArticleContent) {
     return (
-      <div
-        style={{
-          fontSize: '18px',
-          paddingTop: '15px',
-          paddingBottom: '10px',
-          textAlign: 'justify'
-        }}
-      >
-        <p>{this.props.fullArticleContent?.content}</p>
+      <div style={{ textAlign: 'center', height: '50vh', marginTop: '25vh' }}>
+        <Spinner animation="grow" size="sm" />{' '}
+        <Spinner animation="grow" size="sm" />{' '}
+        <Spinner animation="grow" size="sm" />{' '}
       </div>
     );
   }
-}
+  return (
+    <div
+      style={{
+        fontSize: '18px',
+        paddingTop: '15px',
+        paddingBottom: '10px',
+        textAlign: 'justify'
+      }}
+    >
+      <p>{props.fullArticleContent?.content}</p>
+    </div>
+  );
+};
 
 function mapStateToProps(state: any) {
   return {
