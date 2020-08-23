e360cdb18b0b275ffe7a747e321f39c75ba28133
Sandipan Ghosh <sandipan.ghosh01@sap.com>
2020-08-23 12:29:47 +0530

    made Home.tsx a functional component

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,31 +2,20 @@ import React, { Suspense } from 'react';
 import SearchBox from './SearchBox';
 const NewsList = React.lazy(() => import('./NewsList'));
 
-interface HomeProps {}
-
-interface HomeState {}
-
-class Home extends React.PureComponent<HomeProps, HomeState> {
-  public constructor(props: HomeProps) {
-    super(props);
-    this.state = {};
-  }
-
-  public render() {
-    return (
-      <div className="outerdiv">
-        <h1 style={{ textAlign: 'center' }}>
-          <strong>UK News</strong>
-        </h1>
-        <SearchBox />
-        <Suspense
-          fallback={<div style={{ textAlign: 'center' }}>Loading...</div>}
-        >
-          <NewsList />
-        </Suspense>
-      </div>
-    );
-  }
-}
+const Home: React.FC = () => {
+  return (
+    <div className="outerdiv">
+      <h1 style={{ textAlign: 'center' }}>
+        <strong>UK News</strong>
+      </h1>
+      <SearchBox />
+      <Suspense
+        fallback={<div style={{ textAlign: 'center' }}>Loading...</div>}
+      >
+        <NewsList />
+      </Suspense>
+    </div>
+  );
+};
 
 export default Home;
